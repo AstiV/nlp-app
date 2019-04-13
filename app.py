4253301022502efd0471d574ed4651ce36e7158f
Astrid Varga <astrid.varga@gmx.de>
2019-04-13 21:33:54 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, url_for
+from flask_bootstrap import Bootstrap
+
+#NLP
+from textblob import TextBlob, Word
+import random
+
+# Configure application
+app = Flask(__name__)
+
+# Routing
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#TODO change this for production! (debug=True only for dev mode)
+if __name__ == '__main__':
+    app.run(debug=True)
+
